4eee66eb5da03ec35a04f966c26997d48cbd1942
moontoon <moon.shresth@gmail.com>
2017-12-18 09:21:45 +0545

    to return list

---
+++ ques5.py
@@ -0,0 +1,8 @@
+#Write a function that simply returns ("Hello", 45, 23.3). Now, call this function and unpack the returned values and print it.
+
+def f():
+    return ["Hello", 45, 23.3]
+
+result = list(f())
+
+print(result)
